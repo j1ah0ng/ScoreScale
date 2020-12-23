0e8b5df695aaa3d3cd9ece0535f928b377e86d90
Jiahong Long <jiahong888long@gmail.com>
2020-12-23 11:50:14 -0800

    Adding CSV functionality.

--- ScoreScale.py
+++ ScoreScale.py
@@ -1,8 +1,23 @@
 import math as m
+import pandas as pd
+import sys
+
+# Special CSV mode.
+if len(sys.argv) > 5 and sys.argv[1] == "--csv":
+    fname = sys.argv[2]
+    df = pd.read_csv(fname)
+    assert("score" in list(df.columns))
+    assert(sys.argv[3] == "--factor")
+    scale = float(sys.argv[4])
+
+    df["score"] = m.ceil(2*df["score"]*scale) / 2
+
+    df.to_csv(fname)
+
 scale = input("Enter the scale factor: ")
 scaleF = float(scale)
 print("begin: ")
 while True:
-	x	= input("Score: ")
-	xFloat	= float(x)
-	print(m.ceil(2*xFloat*scaleF)/2)
+    x = input("Score: ")
+    xFloat = float(x)
+    print(m.ceil(2*xFloat*scaleF)/2)
